f6b63eb42478d1f781936c3a4bf327ff078ae740
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2026-07-19 20:03:46 -0600

    add footer

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const RootLayout = () => {
   return (
-    <>
+    <div className="flex min-h-screen flex-col">
       <Header />
-      <Outlet />
-    </>
+      <div className="flex-1">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
   );
 };
 
